92785387d183ceac4b60c8dfb0c6951e8fe44ac2
Jai <jaikumarsingh1908@gmail.com>
2020-04-24 18:25:55 +0530

    Signed Cookies

--- app.js
+++ app.js
@@ -30,44 +30,66 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+/* for this cookie-parser we will be using signed cookies.
+ So, for this cookie-parser, supply a secret key as
+ the parameter here. The secret key could be any string there
+ it's just a key that can be used by our cookie-parser in order to 
+ encrypt the information and sign the cookie that is sent from the server to the client. */
+app.use(cookieParser('12345-67890-09876-54321'));
 //Authentication Process
 
 function auth(req,res,next)
 {
-  console.log(req.headers);//to see what is comming from client side
+  console.log(req.signedCookies);//to see what is comming from client side
 
-  var authHeader = req.headers.authorization;//getting hold of authorization header
-  if(!authHeader)//if authHeader is null the we challenge the client
+  if(!req.signedCookies.user/*user is a property of signed cookies*/)//if user is not authenticated by signed cookie yet then look for authorisation header
   {
-    var err = new Error('You are not authenticated!');
-    res.setHeader('WWW-Authenticate','Basic');
-    err.status = 401;//un-authorize user
-    return next(err);//directly jumps to error handler and sends reply message back to client
+    var authHeader = req.headers.authorization;//getting hold of authorization header
+    if(!authHeader)//if authHeader is null the we challenge the client
+    {
+      var err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate','Basic');
+      err.status = 401;//un-authorize user
+      return next(err);//directly jumps to error handler and sends reply message back to client
+    }
+  
+    //authHeader is string so we split the value and give encoding of buffer i.e base64
+  /*[1] in below code means spliting the array into 2 parts ,1st contains basic and 2nd containf
+   username and pswd in base64 encoding formate*/
+   /*After we obtain username and pswd from second part if array and then we convert it into string
+    which is of formate username:pswd ,therefore we are again splitting it by colen to get seperate 
+    username and pswd*/
+      var auth = new Buffer.from(authHeader.split(' ')[1],'base64').toString().split(':');
+    //auth array contains two part username and pswd
+      var username = auth[0];
+      var password  = auth[1];
+  
+    //using default value for user name and pswd
+  
+      if(username === 'admin' && password === 'password')
+      {
+        res.cookie('user' ,'admin',{signed : true});//setting up the cookie
+        /*Setting up the cookied as name 'user' and value 'admin' and making it as signed cookie */
+        next();//next, this means that from the auth their request will passed on the next set of middleware here and then Express will try to match the specific request to middleware which will service the request 
+      }
+      else{
+        var err = new Error('You are not authenticated!');
+        res.setHeader('WWW-Authenticate','Basic');
+        err.status = 401;//un-authorize user
+        return next(err);
+      }
   }
-
-  //authHeader is string so we split the value and give encoding of buffer i.e base64
-/*[1] in below code means spliting the array into 2 parts ,1st contains basic and 2nd containf
- username and pswd in base64 encoding formate*/
- /*After we obtain username and pswd from second part if array and then we convert it into string
-  which is of formate username:pswd ,therefore we are again splitting it by colen to get seperate 
-  username and pswd*/
-  var auth = new Buffer(authHeader.split(' ')[1],'base64').toString().split(':');
-  //auth array contains two part username and pswd
-  var username = auth[0];
-  var password  = auth[1];
-
-  //using default value for user name and pswd
-
-  if(username === 'admin' && password === 'password')
+  else//if user already exists
   {
-    next();//next, this means that from the auth their request will passed on the next set of middleware here and then Express will try to match the specific request to middleware which will service the request 
-  }
-  else{
-    var err = new Error('You are not authenticated!');
-    res.setHeader('WWW-Authenticate','Basic');
-    err.status = 401;//un-authorize user
-    return next(err);
+    if(req.signedCookies.user === 'admin')
+    {
+      next();
+    }
+    else{
+      var err = new Error('You are not authenticated!');
+      err.status = 401;//un-authorize user
+      return next(err);
+    }
   }
 }
 app.use(auth);//auth function which is added
